fda746d735c6ddb27f5b7a62fcf14043c2589026
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-05-09 10:28:26 +0200

    added playercontroller

---
+++ Axis-12/Assets/scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float fMoveSpeed;
+    public float fJumpHeight;
+    public int iMaxAmmo = 10;
+    float fMoveIn;
+    bool bIsFacingRight;
+    int iCurrentAmmo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fMoveIn = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3(fMoveIn, rb.velocity.y, 0);
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
