b2fbc3b7bc4a0c8c715f6a1656a3078d487d98af
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-05-16 11:44:36 +0200

    asd

---
+++ Axis-12/Assets/FuserScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuserScript : Enemy
+{
+   
+    bool bIsMoving = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        bIsFacingRight = false;
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine("Turn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bIsMoving&&bIsFacingRight)
+        {
+            rb.velocity = transform.right * fMoveSpeed;
+        }else if (bIsMoving && !bIsFacingRight)
+        {
+            rb.velocity = -transform.right * fMoveSpeed;
+        }
+        if (Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) <= 3)
+        {
+            Boom();
+            Debug.Log("boom");
+        }
+        sr.flipX = bIsFacingRight;
+    }
+
+    private IEnumerator Turn(float waitTime)
+    {
+        bIsMoving = false;
+        yield return new WaitForSeconds(waitTime);
+        Debug.Log("turning");
+        bIsMoving = true;
+        bIsFacingRight = !bIsFacingRight;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            StartCoroutine(Turn(2f));
+        }
+    }
+    void Boom()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<HealthGen>().iHP -= 3;
+        Destroy(gameObject);
+    }
+}
