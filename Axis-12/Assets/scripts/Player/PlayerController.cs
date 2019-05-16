39aa1bbe334a19864a48976ba2b62c95e751aa78
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-05-16 11:59:03 +0200

    ads

---
+++ Axis-12/Assets/scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float fMoveSpeed;
+    public Transform check;
+    public float fJumpHeight;
+    public LayerMask ground;
+    public int iMaxAmmo = 10;
+    float fMoveIn;
+    bool bIsFacingRight;
+    bool bIsGrounded;
+    
+    int iCurrentAmmo;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bIsGrounded = Physics2D.OverlapCircle(check.position, 0.1f, ground);
+        fMoveIn = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3(fMoveIn*fMoveSpeed, rb.velocity.y, 0);
+        if (Input.GetKeyDown(KeyCode.Space)&&bIsGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, fJumpHeight, 0);
+        }
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
