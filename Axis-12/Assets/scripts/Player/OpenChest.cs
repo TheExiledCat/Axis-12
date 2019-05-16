4c4234bc78c28069b5dc1b7ec6778e69c4bb0235
yomo-kun <24764@ma-web.nl>
2019-05-16 11:09:35 +0200

    Shoot

---
+++ Axis-12/Assets/scripts/Player/OpenChest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChest : MonoBehaviour
+{
+
+    public GameObject Chest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private IEnumerator OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ChestTrigger")
+        {
+            Debug.Log("OwO uve touched the blockywocky <3");
+            yield return StartCoroutine("OpeningChest");
+            
+        }
+    }
+
+ 
+
+    private IEnumerator SpawningKey()
+    {
+        //Key spawn animation
+        yield return new WaitForSeconds(3);
+    }
+
+    private IEnumerator OpeningChest()
+    {
+        Debug.Log("opening the chest to getchu the key");
+        //play chest opening animation
+        yield return StartCoroutine("SpawningKey");
+        yield return new WaitForSeconds(3);
+        Chest.active = false;
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Axis-12/Assets/scripts/Player/PlayerController.cs
+++ Axis-12/Assets/scripts/Player/PlayerController.cs
@@ -18,6 +18,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         rb = GetComponent<Rigidbody2D>();
     }
 
