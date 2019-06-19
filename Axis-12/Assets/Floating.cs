02813935260e1192e261fcce146597de41685ae1
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2019-06-19 12:25:31 +0200

    sad

---
+++ Axis-12/Assets/Floating.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine("Up");
+    }
+    IEnumerator Up()
+    {
+        for(int i = 0; i < 15; i++)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.05f);
+            yield return new WaitForEndOfFrame();
+        }
+        StartCoroutine("Down");
+    }
+    IEnumerator Down()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.05f);
+            yield return new WaitForEndOfFrame();
+        }
+        StartCoroutine("Up");
+    }
+}
